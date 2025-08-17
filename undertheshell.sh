bf97bd6e49a446ad2d1d2f9b6a55f84bec8904ac
Vedant Parankar <parankarvs.21@gmail.com>
2025-08-17 07:09:09 +0000

    Basic Grid and Nav

--- undertheshell.sh
+++ undertheshell.sh
@@ -2,20 +2,46 @@
 
 clear
 tput setaf 3
-for((x=0;x<10;x++)); do
-  tput cup 0 $x
-  echo -n "#"
-done
-for((y=1;y<9;y++)); do
-  tput cup $y 0
-  echo -n "#"
-  tput cup $y 9
-  echo -n "#"
-done
-for((x=0;x<10;x++)); do
-  tput cup 9 $x
-  echo -n "#"
+
+height=10
+width=10
+posx=5
+posy=5
+
+stty -echo -icanon time 0 min 0
+while true; do
+  clear
+  for((y=0;y<height;y++)); do
+    for((x=0;x<width;x++)); do
+      if [[ $x -eq $posx && $y -eq $posy ]]; then
+        tput setaf 1
+        echo -n "@"
+        tput setaf 3
+      else
+        echo -n "."
+      fi
+    done
+    echo
+  done
+  echo
+
+  read -n1 key
+
+  case "$key" in
+    a) ((posx--));;
+    d) ((posx++));;
+    w) ((posy--));;
+    s) ((posy++));;
+    q) break;;
+  esac
+
+  ((posx<0)) && posx=0
+  ((posx>=width)) && posx=$((width-1))
+  ((posy<0)) && posy=0
+  ((posy>=height)) && posy=$((height-1))
+  
+  sleep 0.05
 done
 
 tput sgr0
-
+stty sane
